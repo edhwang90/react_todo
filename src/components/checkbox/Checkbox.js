891a9cbddf852d3aca1f352f6751c7ab32fa6cbc
EDH <ambp@Apples-MacBook-Pro.local>
2020-04-23 15:38:36 -0400

    checkbox refactor
    checkbox refactor

--- src/components/checkbox/Checkbox.js
+++ src/components/checkbox/Checkbox.js
@@ -3,16 +3,17 @@ import React from 'react';
 import './Checkbox.scss';
 
 export const Checkbox = (props) => {
-  const { id, label, toggleProp, handleToggle } = props;
+  const { id, label, toggleProp, handleToggle, disabled } = props;
 
   return (
-    <React.Fragment>
+    <div className="checkbox-container">
       <input className="checkbox" type="checkbox" 
       checked={toggleProp} 
       id={'checkbox' + id} 
       onChange={handleToggle}
+      disabled={disabled}
       />
       <label htmlFor={'checkbox' + id}><span>{label}</span></label>
-    </React.Fragment>
+    </div>
   )
 }
